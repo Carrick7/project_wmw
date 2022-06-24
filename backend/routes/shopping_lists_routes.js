a57dee9faa457ca077584f573c75cbfa1fa50b21
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-06-24 17:02:30 +0100

    added GET single shopping lsit method

--- backend/routes/shopping_lists_routes.js
+++ backend/routes/shopping_lists_routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getShoppingLists, postShoppingLists, putShoppingLists, deleteShoppingLists} = require('../controllers/shopping_list_controller');
+const { getShoppingLists, postShoppingLists, putShoppingLists, deleteShoppingLists, getSingleShoppingList} = require('../controllers/shopping_list_controller');
 //Authentication middleware for shopping lists
 const { protect } = require('../middleware/auth_middleware');
 
 //Shopping Lists Routes - GET, POST, PUT, DELETE
 //GET + POST shopping lists 
 router.route('/').get(protect, getShoppingLists).post(protect, postShoppingLists);
-//PUT + DELETE shopping lists 
-router.route('/:id').put(protect, putShoppingLists).delete(protect, deleteShoppingLists);
+//PUT + DELETE shopping lists & GET single list
+router.route('/:id').put(protect, putShoppingLists).delete(protect, deleteShoppingLists).get(protect, getSingleShoppingList);
 
 module.exports = router;
