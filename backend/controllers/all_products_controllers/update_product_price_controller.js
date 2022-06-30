7d67ffcbb590ae2cab0826efd15acba69426a4c9
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-06-30 23:46:38 +0100

    optimised all_products controller and tested every request

--- backend/controllers/all_products_controllers/update_product_price_controller.js
+++ backend/controllers/all_products_controllers/update_product_price_controller.js
@@ -1,22 +1,61 @@
 const asyncHandler = require('express-async-handler');
 //Models Needed for this controller
 const All_products = require('../../model/all_products_model');
+// Validation Imports from Regex Middleware
+const { validateSale } = require('../../middleware/regex_middleware');
 
 //Description: Add a historical price to a product
 //Route: PUT /api/all_products/:barcode/:shop
 //Access: Public
 const updateProductPrice = asyncHandler(async (req, res) => {
   const product = await All_products.findOne({barcode: req.params.barcode, shop: req.params.shop}); 
+  const { historical_prices } = req.body;
 
+  // Ensuring that the product exists
   if (!product) {
-    res.status(404)
-    throw new Error('Product not found');
+    return res.status(404).send(
+      {message: `This product doesn't exist`});
+   }
+
+ // Ensuring that 1 update is done at a time
+ if(historical_prices.length > 1){
+    return res.status(400).send(
+      {message: `Only one price & sale point can be added at a time`});
+ }
+
+  // Validate the sale input
+  // Ensuring that the sale field is filled out
+  if(!historical_prices[0].sale){
+    return res.status(400).send(
+      {message: `Please fill out the sale input field`});
   }
 
+  // Validate the sale input (ENUM)
+  const lowerCaseSales = historical_prices[0].sale.toLowerCase();
+  if(validateSale.indexOf(lowerCaseSales) === -1) {
+    return res.status(400).send(
+     {message: `Yes or No are the only valid option for the sale field`});
+  }  
+
+  // Validate the price_per_unit input
+  // Ensuring that the price_per_unit field is filled out
+  if(!historical_prices[0].price_per_unit){
+    return res.status(400).send(
+     {message: `Please fill out the price per unit field`});    
+  }
+
+  // Validate the price_per_unit input (ENUM)
+  const price = historical_prices[0].price_per_unit;
+  if(price <= 0.01 || price > 1000 ){
+    return res.status(400).send(
+     {message: `The price per of a product must be at least €0.01 or not more than €1000`});    
+  }
+
+  // Finding the product and updating the historical_prices
   const updatedProductPrice = await All_products.findOneAndUpdate({barcode: req.params.barcode, shop: req.params.shop}, 
     // $push is used to add a new object to the array without deleting previous objects
     {$push: {historical_prices: req.body.historical_prices}}, {new: true});
-
+  
   res.status(200).json(updatedProductPrice);
 });
 
--- backend/model/all_products_model.js
+++ backend/model/all_products_model.js
@@ -5,12 +5,15 @@ const product_namesScehma = new mongoose.Schema({
   generic_name: { 
     type: String,
     required: [true, 'A generic name is required. IE "bananas"'],
+    immutable: true
   },
   official_name: { 
     type: String,
     required: [true, 'The official brand and name are required. IE "Chiquita Brands International"'],
+    immutable: true
   } 
 });
+
 // Schema for embedded data for the different prices a product can have
 const product_price_infoScehma = new mongoose.Schema([{
   sale: { 
@@ -24,8 +27,19 @@ const product_price_infoScehma = new mongoose.Schema([{
     min: [0.01],
     max: [1000],
     required: [true, 'The price per unit is required'], 
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now()
+  },
+  updatedAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now()
   }
-}], { timestamps: true });
+}]
+);
 
 // Schema for Product info
 const all_productsSchema = new mongoose.Schema({
@@ -34,20 +48,23 @@ const all_productsSchema = new mongoose.Schema({
     type: String,
     enum: ['fruit', 'vegetable', 'dairy', 'meat', 'fish/Seafood', 'grains', 'drinks', 'sweets/biscuits', 'toiletries', 'home essentials', 'other'],
     required: [true, 'The category is required'],
-    lowercase: true
+    lowercase: true,
+    immutable: true
   },
   historical_prices: [product_price_infoScehma],
   shop: {
     type: String,
     enum: ['aldi', 'lidl', 'spar', 'tesco', 'eurospar', 'donnybrook fair', 'fresh', "joyce's", 'supervalu', 'dunnes', 'waitrose', 'iceland', 'marks & spencer', 'centra', 'londis', 'mace', 'gala', 'daybreak', 'costcutter', 'other'],
     required: [true, 'The shop is required'],
-    lowercase: true
+    lowercase: true,
+    immutable: true
   },
   barcode: {
     type: Number,
     min: [100000000000],
     max: [9999999999999],
     required: [true, 'The barcode is required'],
+    immutable: true
   }} 
 );
 
