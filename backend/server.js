34d1487b6b76d905fa76a7ff05575dbdd89d0e29
Carrick7 <92592671+Carrick7@users.noreply.github.com>
2022-08-03 17:49:01 +0100

    deployment tb 9

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
   );
 } else{
-  app.get('/', (req, res) => {
-    res.send('Set to Production');
-  });
+  app.get('/', (req, res) =>
+    res.send('Set to Production'));
 }
 
 //error handlers
